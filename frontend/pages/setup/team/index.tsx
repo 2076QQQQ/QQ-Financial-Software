0df6f5ef8a2d9e3699a516289ff1f7ce4ec1e3b1
2076QQQQ <3153520738@qq.com>
2025-12-23 00:26:10 +0800

    4

--- frontend/pages/setup/team/index.tsx
+++ frontend/pages/setup/team/index.tsx
@@ -1,11 +1,2 @@
-import MainLayout from '@/components/MainLayout';
-// 引入你的组件
-import TeamManagement from './TeamManagement'; 
-
-export default function TeamPage() {
-  return (
-    <MainLayout>
-      <TeamManagement />
-    </MainLayout>
-  );
-}
+import TeamManagement from './TeamManagement';
+export default TeamManagement;
